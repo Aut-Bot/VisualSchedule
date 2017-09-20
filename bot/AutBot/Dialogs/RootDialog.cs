e8ba1fe10892a65613f9e084748d8984e944705f
bindukaladeepan <bindukaladeepan@gmail.com>
2017-09-19 17:07:17 -0700

    Added image upload capability

--- bot/AutBot/Dialogs/RootDialog.cs
+++ bot/AutBot/Dialogs/RootDialog.cs
@@ -10,13 +10,14 @@
     using Microsoft.Bot.Builder.Luis;
     using Microsoft.Bot.Builder.Luis.Models;
     using Microsoft.Bot.Connector;
+    using Newtonsoft.Json.Linq;
 
     [LuisModel("950f6fb1-1e16-4478-a881-6e99ac10d1d2", "23c46a5193774e40a28d8f0fa451964a")]
     [Serializable]
     public class RootLUISDialog : LuisDialog<object>
     {
         string name;
-        DateTime schedule;
+        string schedule;
         private const string Yes = "Yes";
         private const string No = "No";
         [LuisIntent("")]
@@ -34,28 +35,59 @@
             context.Wait(this.MessageReceived);
         }
         [LuisIntent("Addtile")]
-        public async Task Schedule(IDialogContext context, LuisResult result)
+        public async Task Addtile(IDialogContext context, LuisResult result)
         {
             //await context.PostAsync("I will add a schedule based on entered text "+ result.Query);
             //context.Wait(this.MessageReceived);
             name = string.Empty;
-            schedule = DateTime.Now;
+            schedule = string.Empty;
+            string starttime = string.Empty;
+            string endtime = string.Empty;
             foreach (var entity in result.Entities)
             {
                 switch (entity.Type)
                 {
                     case "Name":
+                        name = entity.Entity;
                         break;
                     case "builtin.datetimeV2.datetime":
+                        foreach (var vals in entity.Resolution.Values)
+                        {
+                            string json = vals.ToString();
+                            JArray a = JArray.Parse(json);
+                            foreach (var item in a)
+                            {
+                                JValue jvaluestartTime = (JValue)item["value"];
+                                starttime = jvaluestartTime.ToString();
+                                
+                            }
+                                                        
+                        }
+                        break;
+                    case "builtin.datetimeV2.datetimerange":
+                        foreach (var vals in entity.Resolution.Values)
+                        {
+                            string json = vals.ToString();
+                            JArray a = JArray.Parse(json);
+                            foreach(var item in a)
+                            {
+                                JValue jvaluestartTime = (JValue)item["start"];
+                                JValue jvalueendTime = (JValue)item["end"];
+                                 starttime = jvaluestartTime.ToString();
+                                 endtime = jvalueendTime.ToString();
+                            }
+                                              
+
+                        }
                         break;
                 }
             }
-            this.ShowOptions(context);
+            this.ShowOptions(context,name,starttime,endtime);
 
         }
-        private void ShowOptions(IDialogContext context)
+        private void ShowOptions(IDialogContext context, string name, string starttime, string endtime)
         {
-            PromptDialog.Choice(context, this.OnOptionSelected, new List<string>() { Yes, No }, "Do you want to add schedule named {Name} for {Date}", "Not a valid option", 3);
+            PromptDialog.Choice(context, this.OnOptionSelected, new List<string>() { Yes, No }, "Do you want to add schedule named "+name+" for start time: "+starttime+ "  end time: "+endtime, "Not a valid option", 3);
         }
         private async Task OnOptionSelected(IDialogContext context, IAwaitable<string> result)
         {
