2cd896c1852754ed75305caec945ed22ab02f277
shahedc <shahedc@gmail.com>
2017-09-21 17:03:59 -0400

    Integrated storage code into web app
    Integrated storage code into web app, using placeholder conn info

--- web/AutWeb/Api/CalendarBotController.cs
+++ web/AutWeb/Api/CalendarBotController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AutWeb.Data;
 using AutWeb.Models.AutModels;
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using Microsoft.Azure;
 
 namespace AutWeb.Api
 {
@@ -36,21 +39,30 @@ namespace AutWeb.Api
             
             if (imageIncluded)
             {
-                // store in blob, store URI for DB record
-                calendarItemWithImage.Description = "[IMG]" + calendarItemWithImage.Description;
+                // store in blob
+                var storedBlobUri = SaveToBlob(
+                    calendarItemWithImage.Description, calendarItemWithImage.ItemImage);
+                calendarItemWithImage.ImageUrl = storedBlobUri;
+
+                // store URI for DB record
+                calendarItemWithImage.Description = "[IMG-UP]" + calendarItemWithImage.Description;
             } else
             {
                 // no image in request
                 calendarItemWithImage.Description = "[NO-IMG]" + calendarItemWithImage.Description;
                 // get image via Bing image search
+                // TODO: 
                 // store Bing image URI for DB record
+                // TODO: 
             }
 
             // create calendarItem
-            var calendarItem = new CalendarItem();
-            calendarItem.Description = calendarItemWithImage.Description;
-            calendarItem.ImageUrl = calendarItemWithImage.ImageUrl;
-            calendarItem.TimeSlot = calendarItemWithImage.TimeSlot;
+            var calendarItem = new CalendarItem
+            {
+                Description = calendarItemWithImage.Description,
+                ImageUrl = calendarItemWithImage.ImageUrl,
+                TimeSlot = calendarItemWithImage.TimeSlot
+            };
 
             // insert DB calendar item (calendarItem.Id, TimeSlot, ImageUrl, Description)
             _context.CalendarItems.Add(calendarItem);
@@ -60,5 +72,34 @@ namespace AutWeb.Api
             return Ok(calendarItem);
         }
 
+        private string SaveToBlob(string blobName, byte[] blobData)
+        {
+            var storageConn = "DefaultEndpointsProtocol=https;AccountName=STORAGE_ACCOUNT_NAME;AccountKey=ACCOUNT_KEY_VALUE;EndpointSuffix=core.windows.net";
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting(storageConn));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("photo");
+
+            //Create a new container, if it does not exist
+            //var exists = container.CreateIfNotExists(); // doesn't work with .NET Core?
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+
+            //// OPTIONAL: set public permissions
+            //container.SetPermissions(
+            //    new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            // Upload Blob From Byte Array (Async)
+            blockBlob.UploadFromByteArrayAsync(blobData, 0, blobData.Length - 1);
+            
+            return blockBlob.StorageUri.PrimaryUri.AbsoluteUri;
+        }
     }
 }
